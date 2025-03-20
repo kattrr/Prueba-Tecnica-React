0e17663f33c77caa161ebf52a270d9f513556317
kathering Rivera <katheriverar@gmail.com>
2025-03-19 20:56:29 -0500

    Agregar Iconos de FontAwesome :cat2:

--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Router from './routes/Router';
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
 import "bootstrap/dist/css/bootstrap.min.css";
+
+library.add(fas)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
