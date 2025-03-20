c94829d8ce9f84c065f38c68ee1ed7ce678526a7
kathering Rivera <katheriverar@gmail.com>
2025-03-20 16:14:10 -0500

    Modificar estilos nav :cat2:

--- src/layouts/nav.js
+++ src/layouts/nav.js
@@ -1,14 +1,23 @@
 import { Link } from "react-router-dom";
 const Nav = () => {
   return (
-    <nav className="navbar ">
-      <div className="container-fluid">
-        <li className="nav-item">
-          <Link to="/">Home</Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/Departamentos">Departamentos</Link>
-        </li>
+    <nav className="navbar navbar-expand-lg py-4" style={{ backgroundColor: "#99bdff" }}>
+      <div className="container">
+        <div className="container-fluid">
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarText">
+            <ul className="navbar-nav fw-bolder">
+              <li className="nav-item">
+                <Link to="/" className="nav-link">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/Departamentos" className="nav-link">Departamentos</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </nav>
   );
