99d2a7f1a2b243f82b994e8e33c131d115f0aefd
kathering Rivera <katheriverar@gmail.com>
2025-03-20 10:26:36 -0500

    Agregar barra de busqueda Departamentos :cat2:

--- src/pages/Departamentos.js
+++ src/pages/Departamentos.js
@@ -4,7 +4,8 @@ import CardItem from '../components/cardItem';
 
 const Departamentos = () => {
     const [departmentsData, setDepartmentsData] = useState(null);
-
+    const [searchTerm, setSearchTerm] = useState("")
+    
     useEffect(() => {
         const fetchData = async () => {
             try {
@@ -16,17 +17,35 @@ const Departamentos = () => {
         };
         fetchData();
     }, []);
-
+    const filteredDepartments = departmentsData?.filter(department =>
+        department.name.toLowerCase().includes(searchTerm.toLowerCase()) // Comparación sin distinción de mayúsculas/minúsculas
+    );
     return (
         <div className="container">
             <h1 className="my-5 text-center">Explora los Departamentos de Colombia</h1>
-            {departmentsData && departmentsData.map(department => (
-                <CardItem 
-                    key={department.id} 
-                    name={department.name} 
-                    description={department.description} 
+            
+            <div className="mb-4 text-center">
+                <input 
+                    type="text" 
+                    placeholder="Buscar departamento..." 
+                    className="form-control w-50 mx-auto"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
                 />
-            ))}
+            </div>
+
+            {/* Mostrar los departamentos filtrados */}
+            {filteredDepartments && filteredDepartments.length > 0 ? (
+                filteredDepartments.map(department => (
+                    <CardItem 
+                        key={department.id} 
+                        name={department.name} 
+                        description={department.description} 
+                    />
+                ))
+            ) : (
+                <p className="text-center">No se encontraron resultados.</p>
+            )}
         </div>
     );
 };
