fcbcaa941807604dddd9c134dbf1f2f60153e058
kathering Rivera <katheriverar@gmail.com>
2025-03-21 13:48:27 -0500

    Agregar estilos y animaciones a la sección de Contacto :cat2:

--- src/pages/Contacto.js
+++ src/pages/Contacto.js
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useState } from 'react';
+import '../styles/Contacto.css';
 
 const Contacto = () => {
     const [formData, setFormData] = useState({ nombre: '', email: '', mensaje: '' });
@@ -40,27 +41,30 @@ const Contacto = () => {
         <>
             <section>
                 <div className="container mt-5 contacto">
-                    <div className="card shadow-sm rounded-5">
-                        <div className="card-body text-center">
-                            <h2 className="card-title" style={{color: 'var(--bs-purple)'}}>Sobre mí</h2>
-                            <p className="card-text text-muted">
-                                <strong>Nombre:</strong> Kathering Rivera Rodriguez
+                    <div className="card shadow-lg rounded-5 playing text-white flex-wrap text-center">
+                        <div class="wave d-none d-sm-block"></div>
+                        <div class="wave d-none d-sm-block"></div>
+                        <div class="wave d-none d-sm-block"></div>
+                        <h1 className="card-title py-3 mt-2" >Sobre mí</h1>
+                        <div className="card-body ">
+                            <h2 className="card-text text-break">
+                                Kathering Rivera Rodriguez
+                            </h2>
+                            <p className="card-text text-light fs-4 text-break">
+                                Desarrolladora Front-End / Diseñadora UX/UI
                             </p>
-                            <p className="card-text text-muted">
-                                <strong>Cargo:</strong> Desarrolladora Front-End / Diseñadora UX/UI
-                            </p>
-                            <div className="d-flex justify-content-center gap-3 mt-3">
-                                <a href="mailto:katheriverar@gmail.com" className="text-decoration-none text-dark">
-                                <FontAwesomeIcon icon="fas fa-envelope" /> Correo
+                            <div className="d-flex justify-content-center gap-3 mt-3 fs-5 flex-wrap">
+                                <a href="mailto:katheriverar@gmail.com" className="text-decoration-none text-light">
+                                    <FontAwesomeIcon icon="fas fa-envelope" /> Correo
                                 </a>
-                                <a href="https://github.com/kattrr" target="_blank" rel="noopener noreferrer" className="text-decoration-none text-dark">
-                                <FontAwesomeIcon icon="fa-brands fa-github" /> GitHub
+                                <a href="https://github.com/kattrr" target="_blank" rel="noopener noreferrer" className="text-decoration-none text-light">
+                                    <FontAwesomeIcon icon="fa-brands fa-github" /> GitHub
                                 </a>
-                                <a href="https://www.linkedin.com/in/katheringriverar/" target="_blank" rel="noopener noreferrer" className="text-decoration-none text-dark">
-                                <FontAwesomeIcon icon="fab fa-linkedin" /> LinkedIn
+                                <a href="https://www.linkedin.com/in/katheringriverar/" target="_blank" rel="noopener noreferrer" className="text-decoration-none text-light">
+                                    <FontAwesomeIcon icon="fab fa-linkedin" /> LinkedIn
                                 </a>
-                                <a href="https://www.behance.net/katherivera" target="_blank" rel="noopener noreferrer" className="text-decoration-none text-dark">
-                                <FontAwesomeIcon icon="fa-brands fa-behance" /> Portafolio
+                                <a href="https://www.behance.net/katherivera" target="_blank" rel="noopener noreferrer" className="text-decoration-none text-light">
+                                    <FontAwesomeIcon icon="fa-brands fa-behance" /> Portafolio
                                 </a>
                             </div>
                         </div>
@@ -69,7 +73,7 @@ const Contacto = () => {
             </section>
             <section>
                 <div className="container my-5 contacto py-5">
-                    <h1 className="text-center" style={{color: 'var(--bs-purple)'}}>Contáctanos</h1>
+                    <h1 className="text-center" style={{ color: 'var(--bs-purple)' }}>Contáctanos</h1>
                     <p className="text-center text-muted">
                         Si tienes alguna pregunta o comentario, no dudes en escribirnos. ¡Nos encantaría saber de ti!
                     </p>
@@ -110,7 +114,7 @@ const Contacto = () => {
                             ></textarea>
                             {errors.mensaje && <div className="invalid-feedback">{errors.mensaje}</div>}
                         </div>
-                        <button type="submit" className="btn w-100 text-white" style={{background: 'var(--bs-purple)'}}>
+                        <button type="submit" className="btn w-100 text-white" style={{ background: 'var(--bs-purple)' }}>
                             Enviar
                         </button>
                     </form>
