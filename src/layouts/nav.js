67d5d45a49233553787eeee72d85867cfb9ef2f5
kathering Rivera <katheriverar@gmail.com>
2025-03-21 14:21:01 -0500

    Agregar resaltado dinámico a los enlaces de navegación y Modificar estilos nav :cat2:

--- src/layouts/nav.js
+++ src/layouts/nav.js
@@ -1,34 +1,37 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+
 const Nav = () => {
+  const location = useLocation();
+
   return (
-    <nav className="navbar navbar-expand-lg py-4 mainNav" style={{ backgroundColor: "#99bdff" }} id="mainNav">
-      <div className="container">
-        <div className="container-fluid">
+    <nav className="navbar navbar-expand-lg py-4 mainNav fw-bolder" style={{ backgroundColor: "#99bdff" }} id="mainNav">
+      <div className="container ">
+        <div className="container-fluid ">
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarText">
-            <ul className="navbar-nav fw-bolder me-auto mb-2 mb-lg-0">
+          <div className="collapse navbar-collapse justify-content-evenly" id="navbarText">
+            <ul className="navbar-nav mb-2 gap-2 gap-md-5 ">
               <li className="nav-item">
-                <Link to="/" className="nav-link">Home</Link>
+                <Link to="/" className={`nav-link ${location.pathname === "/" ? "active bg-primary rounded text-light" : ""}`}>Inicio</Link>
               </li>
               <li className="nav-item">
-                <Link to="/Departamentos" className="nav-link">Departamentos</Link>
+                <Link to="/Departamentos" className={`nav-link ${location.pathname === "/Departamentos" ? "active bg-primary rounded text-light" : ""}`}>Departamentos</Link>
               </li>
               <li className="nav-item">
-                <Link to="/Presidentes" className="nav-link">Presidentes</Link>
+                <Link to="/Presidentes" className={`nav-link ${location.pathname === "/Presidentes" ? "active bg-primary rounded text-light" : ""}`}>Presidentes</Link>
               </li>
               <li className="nav-item">
-                <Link to="/Turismo" className="nav-link">Turismo</Link>
+                <Link to="/Turismo" className={`nav-link ${location.pathname === "/Turismo" ? "active bg-primary rounded text-light" : ""}`}>Turismo</Link>
               </li>
               <li className="nav-item">
-                <Link to="/Aeropuertos" className="nav-link">Aeropuertos</Link>
+                <Link to="/Aeropuertos" className={`nav-link ${location.pathname === "/Aeropuertos" ? "active bg-primary rounded text-light" : ""}`}>Aeropuertos</Link>
               </li>
               <li className="nav-item">
-                <Link to="/Platos" className="nav-link">Platos Tipicos</Link>
+                <Link to="/Platos" className={`nav-link ${location.pathname === "/Platos" ? "active bg-primary rounded text-light" : ""}`}>Platos Tipicos</Link>
               </li>
               <li className="nav-item">
-                <Link to="/Contacto" className="nav-link">Contacto</Link>
+                <Link to="/Contacto" className={`nav-link ${location.pathname === "/Contacto" ? "active bg-primary rounded text-light" : ""}`}>Contacto</Link>
               </li>
             </ul>
           </div>
